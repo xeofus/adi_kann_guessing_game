a9f665c4678b0608b414003d6fdb4a651fc45308
xeofus <xeofus@gmail.com>
2024-11-23 14:02:07 -0700

    Pseudo for each class | Little improvement: getIntWithinRange() method refactored to reduce redundant calls to getInt

--- src/fit.cis5100.akann2024/Console.java
+++ src/fit.cis5100.akann2024/Console.java
@@ -5,34 +5,25 @@ import java.util.Scanner;
 
 public class Console {
 
-    public static int getInt(Scanner sc, String prompt) {
+    public static int getIntWithinRange(Scanner sc, String prompt, int min, int max) {
         int i = 0;
         boolean isValid = false;
         while (!isValid) {
             try {
                 System.out.print(prompt);
                 i = sc.nextInt();
-                isValid = true;
+
+                if (i <= min) {
+                    System.out.println("Error! Number must be greater than " + min);
+                } else if (i >= max) {
+                    System.out.println("Error! Number must be less than " + max);
+                } else {
+                    isValid = true;
+                }
             } catch (InputMismatchException e) {
                 System.out.println("Error! Invalid integer value. Try again.");
             } finally {
-                sc.nextLine(); // discard any other data entered on the line
-            }
-        }
-        return i;
-    }
-
-    public static int getIntWithinRange(Scanner sc, String prompt, int min, int max) {
-        int i = 0;
-        boolean isValid = false;
-        while (!isValid) {
-            i = getInt(sc, prompt);
-            if (i <= min) {
-                System.out.println("Error! Number must be greater than " + min);
-            } else if (i >= max) {
-                System.out.println("Error! Number must be less than " + max);
-            } else {
-                isValid = true;
+                sc.nextLine(); // discard invalid input
             }
         }
         return i;
